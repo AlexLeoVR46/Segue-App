34fdaba95650661c7a8edcadb5d5f58a712d9748
Alexei <motogp1985@gmail.com>
2019-04-24 22:37:30 +0300

    Added appear keyboard

---
+++ Segue/SecondViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SecondViewController.swift
+//  Segue
+//
+//  Created by home on 4/24/19.
+//  Copyright © 2019 home. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
--- Segue/ViewController.swift
+++ Segue/ViewController.swift
@@ -9,12 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var clickedTextField = UITextField()
+    let username = "lessonOne"
+    let password = "23041019"
+    
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var userNameTextField: UITextField!
+    
+    @IBOutlet var logInLabelButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       logInLabelButton.layer.cornerRadius = 5
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+    // Фунция скрытия клавиатуры по тапу на экране
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
     }
 
-
+    @IBAction func resultPressButton(_ sender: Any) {
+        
+    }
 }
 

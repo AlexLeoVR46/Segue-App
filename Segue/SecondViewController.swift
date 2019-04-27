e386e1469fa354c734c9428644d7bac21399956a
Alexei <motogp1985@gmail.com>
2019-04-28 00:26:32 +0300

    Fix constraints and improve code

--- Segue/SecondViewController.swift
+++ Segue/SecondViewController.swift
@@ -12,15 +12,41 @@ class SecondViewController: UIViewController {
 
     @IBOutlet var resultTextField: UILabel!
     
-    var recieveResult = ""
+    let username = "lessonOne"
+    let passwordIn = "23041019"
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        resultTextField.text = recieveResult
-        
+    var currentLogin = ""
+    var currentPassword = ""
+    var result = ""
+    
+    override func viewWillAppear(_ animated: Bool) {
+        doAction(action: result)
+    }
+    
+    //Func of Identity verification, login and password
+    private func identityCheck(currentLogin: String, currentPassword: String) -> Bool {
+        return currentLogin == username && currentPassword == passwordIn
     }
     
     
-
+    //Func of perform action depending on the button click "LogIn" or "Forgot password"
+    private func doAction(action: String) {
+        switch action {
+        case "loginSegue":
+            if identityCheck(currentLogin: currentLogin, currentPassword: currentPassword) {
+                resultTextField.text = "Access successfull"
+                resultTextField.textColor = .green
+            }
+            else
+            {
+                resultTextField.text = "Your login or password is incorrect"
+                resultTextField.textColor = .red
+            }
+        case "passUsernameSegue":
+            resultTextField.text = "Correct password - \(passwordIn) and login - \(username) "  
+        default:
+            resultTextField.text = "Unknown action"
+        }
+    }
 
 }
--- Segue/ViewController.swift
+++ Segue/ViewController.swift
@@ -10,16 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let username = "lessonOne"
-    let password = "23041019"
-    var nameTextName = ""
-    
-
-    
     @IBOutlet var passwordTextField: UITextField!
     @IBOutlet var userNameTextField: UITextField!
     
-
     @IBOutlet var logInLabelButton: UIButton!
     
     override func viewDidLoad() {
@@ -28,18 +21,15 @@ class ViewController: UIViewController {
         
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
-        
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
-        view.addGestureRecognizer(tap)
     }
     
-    
-    // Фунция скрытия клавиатуры по тапу на экране
-    @objc func dismissKeyboard() {
+    // The function of hiding the keyboard by tap on the screen
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
         view.endEditing(true)
     }
     
-    //Функция появления клавиатуры и смещения textField
+    //Function show and hide keyboard
     @objc func keyboardWillShow(notification: NSNotification) {
         if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
             if self.view.frame.origin.y == 0 {
@@ -47,7 +37,6 @@ class ViewController: UIViewController {
             }
         }
     }
-
     @objc func keyboardWillHide(notification: NSNotification) {
         if self.view.frame.origin.y != 0 {
             self.view.frame.origin.y = 0
@@ -55,24 +44,19 @@ class ViewController: UIViewController {
     }
     
     
-    @IBAction func fogotPassOrUsernameAction(_ sender: Any) {
-        
-    }
-    //  Функция передачи данных на второй вью-контроллер при нажатии на button
-    @IBAction func resultFromPressButton(_ sender: Any) {
-        self.nameTextName = userNameTextField.text!
-        
-        performSegue(withIdentifier: "resultSegue", sender: self)
-    }
-    
+    // Function of data transfer  to the second View controller 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
-        let vc = segue.destination as! SecondViewController
-        vc.recieveResult = self.nameTextName
+        guard let destination = segue.destination as? SecondViewController else {return}
+        guard let action = segue.identifier else {return}
+        
+        destination.currentLogin = userNameTextField.text ?? ""
+        destination.currentPassword = passwordTextField.text ?? ""
+        destination.result = action
+        
+        self.view.endEditing(true)
+        
     }
-    
-
-   
     
 
 }
